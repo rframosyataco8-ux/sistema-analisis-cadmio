69516bde170f18dc447565b4e16b702122f8191e
Fabricio_Ramos <rframosyataco8@gmail.com>
2026-07-28 21:36:38 -0500

    Configurar Electron para abrir como app de escritorio real (ventana nativa)

---
+++ desktop/electron/main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 700,
+    backgroundColor: '#030712',
+    title: 'Sistema Inteligente de Análisis de Cadmio',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    show: false,
+  });
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    // win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
